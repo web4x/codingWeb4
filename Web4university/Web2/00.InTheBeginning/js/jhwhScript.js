a5537cec2a13cfaeecdadad1b103c781dd6ab0f2
mdonges <marcel.donges@gmail.com>
2023-10-05 11:04:13 +0200

    added democraticIphone

---
+++ Web4university/Web2/00.InTheBeginning/js/jhwhScript.js
@@ -0,0 +1,21 @@
+var word = {};
+
+word.j = 0;
+word.h = 0.01;
+
+word.speak = function breath() {
+  while (word.j < 2 * Math.PI) {
+    word.j += word.h;
+    word.w = Math.sin(word.j);
+    console.log("j:", this.j);
+  }
+  this.j = 0;
+};
+word.speak();
+
+word.live = function eternity() {
+  while (this.j > 0) {
+    this.speak();
+  }
+};
+word.live();
