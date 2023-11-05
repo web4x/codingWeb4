f27f43602f56385d645efef59e78c0e833d4d722
mdonges <marcel.donges@gmail.com>
2023-11-05 12:33:21 +0100

    added DONTS

--- docs/Web4university/Web2/00.InTheBeginning/js/jhwhScript.v2.js
+++ docs/Web4university/Web2/00.InTheBeginning/js/jhwhScript.v2.js
@@ -1,18 +1,23 @@
 let word = {};
 
+// check with profiler if this makes a diffrence
+// add lesson how to profile with DevTools
 const twoPI = 2 * Math.PI
+
+// DONT global variables
 //var globalVariable = "globalVariable";
 
-
+// DONT just modify objects arbitarrily
 word.j = 0.01;
 word.h = -Math.PI;
+// DONT misspelled creates new element
+// word.i = 0.01;
 
+// DONT use hard code HTML-tag IDs
 jotaView.value = word.j;
 
 
 word.speak = function breath() {
-
-  
   // DONT calculate 2 * Math.PI each time
   while (word.j < 2 * Math.PI) {
   // DONT use global reference twoPI
@@ -23,7 +28,7 @@ word.speak = function breath() {
     //console.log("j:", this.j);
     
     // DONT use hard coded HTML ids
-    heightView.value = word.h;
+    jotaView.value = word.h;
     heightView.value = word.h;
     widthView.value = word.w;
   }
